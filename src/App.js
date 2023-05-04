3bd116e51a0cda3d885f8c77ddc0577592c8391f
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-05-04 14:40:35 +0200

    login fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './assets/scss/app.min.css';
-import OnboardingPartial2 from './components/partials/onboardingPartials/OnboardingPartial2';
-import Onboarding from './components/views/Onboarding';
+import "./assets/scss/app.min.css";
+import Login from "./components/views/Login";
+import Onboarding from "./components/views/Onboarding";
 function App() {
   return (
     <>
-    <Onboarding/>
+      <Login />
     </>
   );
 }
 
 export default App;
-   
--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,18 +1,49 @@
 //imports
-import LoginPartial from "../partials/loginPartials/LoginPartial";
-import SocialMediaPartial from "../partials/registrationPartials/SocialMediaPartial"
-
+import BackArrow from "../partials/BackArrow";
+import VerticalBar from "../partials/VerticalBar";
+import SocialMediaPartial from "../partials/registrationPartials/SocialMediaPartial";
 
 const Login = () => {
-    return(
+  return (
     <>
-    
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+            <div className="RegHeader">
+              <BackArrow />
+              <h3>Login</h3>
+            </div>
 
-        <LoginPartial/>
-        <SocialMediaPartial/>
-
-
+            <VerticalBar />
+            <h2 className="text-center my-4">Login</h2>
+            <form method="post">
+              <div className="input-field-group">
+                <label>EMAIL</label>
+                <input
+                  type="email"
+                  className="input-field"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="input-field-group">
+                <label>PASSWORD</label>
+                <input
+                  type="password"
+                  className="input-field"
+                  placeholder="Password"
+                />
+              </div>
+              <button className="BigBlackButton" type="submit">
+                Login
+              </button>
+            </form>
+            <p className="text-center my-2">Already have an account?</p>
+            {/* Add SIGN IN and links to FACEBOOK, etc. */}
+          </div>
+        </div>
+      </div>
+      <SocialMediaPartial />
     </>
-    )
-}
+  );
+};
 export default Login;
