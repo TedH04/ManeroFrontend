95419a437db447a85a83d1610c0e3b4742858071
HenrikHardsjo <heha98@live.se>
2023-05-04 09:13:14 +0200

    OnboardingNamefix

--- src/components/views/Onboarding.js
+++ src/components/views/Onboarding.js
@@ -1,6 +1,5 @@
 //imports
 import OnboardingPartial2 from "../partials/onboardingPartials/OnboardingPartial2";
-import OnboardingPartial1 from "../partials/onboardingPartials/OnbardingPartial1";
 
 
 
